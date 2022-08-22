e39e76905b8919872f66727204cb2aac49cb5670
Abhishek Rai <abhishek.rai8992@gmail.com>
2022-08-22 23:44:03 +0530

    Download Match Videos from Site:- volleymetrics.hudl.com

--- DVW_Works.py
+++ DVW_Works.py
@@ -1,5 +1,5 @@
 import requests
-
+# Set User Agent
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) ' \
              'Chrome/87.0.4280.141 Safari/537.36'
 
